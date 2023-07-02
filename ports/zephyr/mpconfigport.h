a19150eac9b5885b5b230f55cb2c8182d50073ff
Mariano Goluboff <goluboff@gmail.com>
2023-07-02 13:19:19 -0400

    Add external flash support.

--- ports/zephyr/mpconfigport.h
+++ ports/zephyr/mpconfigport.h
@@ -69,7 +69,7 @@
 #define MICROPY_PY_MACHINE_SPI_MSB (SPI_TRANSFER_MSB)
 #define MICROPY_PY_MACHINE_SPI_LSB (SPI_TRANSFER_LSB)
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW mp_pin_make_new
-#define MICROPY_PY_STRUCT           (0)
+#define MICROPY_PY_STRUCT           (1)
 #ifdef CONFIG_NETWORKING
 // If we have networking, we likely want errno comfort
 #define MICROPY_PY_UERRNO           (1)
